f2c3160e4b63941f55d3a678911063e11bbf4789
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-05-13 22:25:17 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var path = require("path");
+const port = 8000;
+
+var app = express();
+
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.use(express.static("./"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "index.html"));
+});
+
+io.on("connection", function (socket) {
+  socket.emit("main", "hello");
+  socket.on("main", (msg) => {
+    console.log(msg)
+    io.emit("main", "echo " + msg);
+  });
+});
+
+http.listen(port || 8000, function () {
+  console.log("Listening");
+});
