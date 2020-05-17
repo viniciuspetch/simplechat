ce499d81848ec48a69e55b95794b5c6b1233ae2b
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-05-17 14:36:25 -0300

    Separated form for username

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,25 @@
 var socket = io();
 var chatIn = document.getElementById("chat-in");
+var usernameForm = document.getElementById("username-form");
+var roomForm = document.getElementById("room -98-form");
 var chatInMsg = document.getElementById("chat-in-msg");
 var usernameInput = document.getElementById("username-input");
 var messages = document.getElementById("messages");
+var username = "";
 
 chatIn.onsubmit = function (e) {
   e.preventDefault();
   var message = chatInMsg.value;
-  var username = usernameInput.value;
   data = { username, message };
   socket.emit("main", data);
   chatInMsg.value = "";
 };
+
+usernameForm.onsubmit = function (e) {
+  e.preventDefault();
+  username = usernameInput.value;
+};
+
 socket.on("main", function (data) {
   var listItem = document.createElement("LI");
   var content = document.createTextNode(data.username + ": " + data.message);
